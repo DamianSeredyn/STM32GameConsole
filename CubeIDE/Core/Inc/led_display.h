276e00a0641968f24fcce3edfde0bf76a6e3b160
unknown <damianseredyn1@gmail.com>
2024-12-12 19:52:36 +0100

    Add led display

---
+++ CubeIDE/Core/Inc/led_display.h
@@ -0,0 +1,14 @@
+/*
+ * led_display.h
+ *
+ *  Created on: Dec 12, 2024
+ *      Author: Damian
+ */
+
+#ifndef INC_LED_DISPLAY_H_
+#define INC_LED_DISPLAY_H_
+
+
+void SPI_Led_init(void);
+
+#endif /* INC_LED_DISPLAY_H_ */
