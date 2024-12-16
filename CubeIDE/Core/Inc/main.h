c994d83a8efbf7c5a76afaa2610b0e760a302cb9
DamianSeredyn <62287667+DamianSeredyn@users.noreply.github.com>
2024-12-16 12:44:52 +0100

    First iteration of LED init

--- CubeIDE/Core/Inc/main.h
+++ CubeIDE/Core/Inc/main.h
@@ -55,6 +55,7 @@ extern "C" {
 #include "spi.h"
 #include "Tim.h"
 #include "buzzer.h"
+#include "led_display.h"
 /* USER CODE END Includes */
 
 /* Exported types ------------------------------------------------------------*/
--- CubeIDE/Core/Src/main.c
+++ CubeIDE/Core/Src/main.c
@@ -98,15 +98,18 @@ int main(void)
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
    InitRingbuffer();
-
    MX_I2C1_Init();
+
+   SPI_ILI9486_init();
+   ILI9486_Init();
 
    MX_SPI2_Init();
    spi_init_interupts();
 
    MX_TIM2_Init();
 
-   PlayMusic(0);
+
+   ILI9486_ReadID();
   /* USER CODE END 2 */
 
   /* Infinite loop */
