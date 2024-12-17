87d06d90db8c75e892ac30ffc5d625dc3f3c254a
Szymon Dłuski <szymon76507@gmail.com>
2024-12-17 23:46:24 +0100

    feat add: add SD_card init prototype, commented playmusic in main because it loops infinitely

---
+++ CubeIDE/Core/Inc/SD_card.h
@@ -0,0 +1,17 @@
+/*
+ * SD_card.h
+ *
+ *  Created on: Dec 17, 2024
+ *      Author: Pc
+ */
+
+#ifndef INC_SD_CARD_H_
+#define INC_SD_CARD_H_
+
+#include "spi.h"
+#include "main.h"
+#include "stdbool.h"
+
+void SD_card_init();
+
+#endif /* INC_SD_CARD_H_ */
